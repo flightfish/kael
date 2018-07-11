1ecbf285f40dac506330506c4cdc9cc530263e63
wangchao <wangchao@knowbox.cn>
2018-07-11 19:40:09 +0800

    tmp

--- dockerconf/dockerinit.sh
+++ dockerconf/dockerinit.sh
@@ -4,4 +4,4 @@ docker run -d --network knowboxplatform --name kael \
 -v /data/wwwroot/kael/dockerconf/ngconf/ngvhost:/usr/local/nginx/conf/vhost \
 -v /data/wwwroot:/data/wwwroot \
 --restart=always \
-php72swoole:201807111504
+php70:201807111930
