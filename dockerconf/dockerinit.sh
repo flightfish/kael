4f70e5d3a048d19b76139d6953ba3c2363702b71
wangchao <wangchao@knowbox.cn>
2018-07-13 16:29:55 +0800

    tmp

--- dockerconf/dockerinit.sh
+++ dockerconf/dockerinit.sh
@@ -1,7 +1,7 @@
-docker stop kael
-docker rm kael
+docker container stop kael
+docker container rm kael
 docker run -d --network knowboxplatform --name kael \
 -v /data/wwwroot/kael/dockerconf/ngconf/ngvhost:/usr/local/nginx/conf/vhost \
 -v /data/wwwroot:/data/wwwroot \
 --restart=always \
-php70:201807112007
+php70:1.0.0
