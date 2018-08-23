2d25bb0513f9a1f62a2254dd2e1c965c43e7d3cc
wangchao <wangchao@knowbox.cn>
2018-08-23 19:57:54 +0800

    tmp

--- dockerconf/dockerinit.sh
+++ dockerconf/dockerinit.sh
@@ -1,7 +1,8 @@
+docker pull docker.inner.knowbox.cn:5000/knowbox-platform/php70:1.0.0-yac
 docker container stop kael
 docker container rm kael
 docker run -d --network knowboxplatform --name kael \
 -v /data/wwwroot/kael/dockerconf/ngconf/ngvhost:/usr/local/nginx/conf/vhost \
 -v /data/wwwroot:/data/wwwroot \
 --restart=always \
-php70:1.0.0-yac
+docker.inner.knowbox.cn:5000/knowbox-platform/php70:1.0.0-yac
