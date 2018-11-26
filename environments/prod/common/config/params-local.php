609121e6bc86a06b4050592e5d954b63380e0103
wangchao <wangchao@knowbox.cn>
2018-11-26 16:21:13 +0800

    tmp

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -3,4 +3,9 @@ return [
     'sendFrom'=>['tikunotice@knowbox.cn'=>'用户中心操作通知'],
     'redis_cache'=>1,
     'redis_cache_time'=>3600,
+
+    'ldap_addr'=>'ldaps://10.9.58.21',
+    'ldap_port'=>636,
+    'ldap_rdn'=>'cn=Manager,dc=kb,dc=com',
+    'ldap_passwd'=>'ldap123_dc',
 ];
