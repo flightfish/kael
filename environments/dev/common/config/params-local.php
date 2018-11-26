9785ad62e10349a64d1db8273be365aad3fd9748
wangchao <wangchao@knowbox.cn>
2018-11-26 10:32:52 +0800

    tmp

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -4,6 +4,7 @@ return [
     'redis_cache'=>1,
     'redis_cache_time'=>3600,
     'ldap_addr'=>'ldaps://10.9.58.21',
+    'ldap_port'=>636,
     'ldap_rdn'=>'cn=Manager,dc=kb,dc=com',
     'ldap_passwd'=>'ldap123_dc',
 ];
