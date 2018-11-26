1054776800987a64e103f1f559616849c7476c66
wangchao <wangchao@knowbox.cn>
2018-11-26 11:09:30 +0800

    tmp

--- console/controllers/LdapController.php
+++ console/controllers/LdapController.php
@@ -17,8 +17,8 @@ class LdapController extends Controller
         if(empty(Yii::$app->params['ldap_addr'])){
             echo "未设置ldap地址\n";exit();
         }
-        echo $caPath = dirname(dirname(dirname(__FILE__))).'/common/config/ca.crt'."\n";
-//        putenv('LDAPTLS_CACERT='.$caPath);
+        echo $caPath = dirname(dirname(dirname(__FILE__))).'/common/config/ca.pem'."\n";
+        putenv('LDAPTLS_CACERT='.$caPath);
         putenv('TLS_CACERT='.$caPath);
         ldap_set_option(NULL, LDAP_OPT_DEBUG_LEVEL, 7);
         echo Yii::$app->params['ldap_addr'].':'.Yii::$app->params['ldap_port']."\n";
