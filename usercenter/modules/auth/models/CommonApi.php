4df5bf9ada56a0b6798fde1658c2d6a88896d93c
liunanx <853405546@qq.com>
2018-12-26 18:16:17 +0800

    test

--- usercenter/modules/auth/models/CommonApi.php
+++ usercenter/modules/auth/models/CommonApi.php
@@ -412,7 +412,7 @@ class CommonApi extends RequestBaseModel
             if(in_array($login_ip,\Yii::$app->params['ip_list'])){
                 Cache::setCache($cacheKey, ['count' => $checkRes],1);
             }else{
-                Cache::setCache($cacheKey, ['count' => $checkRes],1);
+                Cache::setCache($cacheKey, ['count' => $checkRes],60);
             }
         }
         //同一手机号一分钟只能发一次
