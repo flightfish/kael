c925d30b07ece84c0db4cf8b64732f4e8b14219f
liunanx <853405546@qq.com>
2018-12-27 11:30:35 +0800

    test

--- usercenter/modules/auth/models/CommonApi.php
+++ usercenter/modules/auth/models/CommonApi.php
@@ -293,7 +293,7 @@ class CommonApi extends RequestBaseModel
             throw new Exception(Exception::MOBILE_CHANGE, Exception::ERROR_COMMON);
         } else {
             if ($user['user_type'] == 0) {
-                $preg = "/^([0-9]*)|([a-zA-Z]*)$/";
+                $preg = "/^([0-9])|([a-zA-Z])$/";
                 if (empty($user['password']) || $user['password'] == md5('123456')||preg_match($preg,$this->user_pass)) {
                     throw new Exception("密码过于简单，请点击忘记密码修改密码", Exception::ERROR_COMMON);
                 }
