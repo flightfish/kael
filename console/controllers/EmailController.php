f7cdca1503c4492beb93afa13faa68d058fc0320
wangchao <wangchao@knowbox.cn>
2019-03-07 16:46:26 +0800

    tmp

--- console/controllers/EmailController.php
+++ console/controllers/EmailController.php
@@ -11,13 +11,17 @@ use yii\console\Controller;
 class EmailController extends Controller
 {
     /**
-     * 初始化用户信息到LDAP
+     * 初始化用户信息到EMAIL
      */
     public function actionUpdate(){
         if(exec('ps -ef|grep "email/update"|grep -v grep | grep -v cd | grep -v "/bin/sh"  |wc -l') > 1){
             echo "is_running";
             exit();
         }
+        //所有公司员工的
+        $allEmail = CommonUser::find()->select('email')
+            ->where(['status'=>0,'user_type'=>0])->andWhere(['!=','email',''])->asArray(true)->column();
+        //所有邮箱的
 
     }
 }
