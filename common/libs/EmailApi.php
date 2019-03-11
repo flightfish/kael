a0e817a23aeb3473b10a0b4d8c19e410ff8adde3
wangchao <wangchao@knowbox.cn>
2019-03-11 17:31:40 +0800

    tmp

--- common/libs/EmailApi.php
+++ common/libs/EmailApi.php
@@ -42,6 +42,7 @@ class EmailApi{
         }
         $retJson = json_decode($retStr,true);
         if(!isset($retJson['errcode']) || 0 != $retJson['errcode']){
+            var_dump($retJson);exit();
             throw new Exception('[EXMAIL]'.$retJson['errmsg']??"");
         }
         return $retJson;
