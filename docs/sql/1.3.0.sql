8ebc7aafd7c4353eb8f621a0703475ace4ba4ab1
liqingshan <592957170@qq.com>
2019-03-28 11:17:50 +0800

    lqs

--- docs/sql/1.3.0.sql
+++ docs/sql/1.3.0.sql
@@ -1,5 +1,2 @@
-CREATE TABLE `user_copy` LIKE `user`;
-INSERT INTO `user_copy` SELECT * FROM `user`;
-
 ALTER TABLE `dingtalk_user`
 ADD COLUMN `kael_id` int(11) NOT NULL DEFAULT '0' COMMENT 'kael账号' after `user_id`;
