adbaad110909c64434511da8c1e2dd07c050da71
liqingshan <592957170@qq.com>
2019-04-01 21:14:51 +0800

    lqs

--- console/controllers/DingController.php
+++ console/controllers/DingController.php
@@ -214,7 +214,7 @@ class DingController extends Controller
                             }
                         }
                         //更新员工关联kael部门  @todo 从主department_id开始向父级依次匹配
-                        $mainDingDepartmentForUser = DepartmentUser::find()->select(['department_id'])->where(['is_main'=>1,'department_id'=>$departmentIds])->scalar();
+                        $mainDingDepartmentForUser = DepartmentUser::find()->select(['depart_id'])->where(['is_main'=>1,'depart_id'=>$departmentIds])->scalar();
                         if(!$mainDingDepartmentForUser && !empty($departmentIds)){
                             $mainDingDepartmentForUser = $departmentIds[0];
                             DepartmentUser::updateAll(['is_main'=>1],['user_id'=>$kaelId,'depart_id'=>$mainDingDepartmentForUser]);
@@ -292,7 +292,7 @@ class DingController extends Controller
                         DepartmentUser::addAllWithColumnRow($cloumns,$rows);
 
                         //更新员工关联kael部门
-                        $mainDingDepartmentForUser = DepartmentUser::find()->select(['department_id'])->where(['is_main'=>1,'department_id'=>$departmentIds])->scalar();
+                        $mainDingDepartmentForUser = DepartmentUser::find()->select(['depart_id'])->where(['is_main'=>1,'depart_id'=>$departmentIds])->scalar();
                         if(!$mainDingDepartmentForUser && !empty($departmentIds)){
                             $mainDingDepartmentForUser = $departmentIds[0];
                             DepartmentUser::updateAll(['is_main'=>1],['user_id'=>$kaelId,'depart_id'=>$mainDingDepartmentForUser]);
