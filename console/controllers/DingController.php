690a3b22c26c1e1fec8559e343d8d40edd16cf12
liqingshan <592957170@qq.com>
2019-04-11 19:30:13 +0800

    lqs

--- console/controllers/DingController.php
+++ console/controllers/DingController.php
@@ -84,9 +84,9 @@ class DingController extends Controller
             foreach ($departmentList as $v) {
                 $userIdList = DingTalkApi::getDepartmentUserIds($v['id']);
                 foreach ($userIdList as $userId){
-//                    if($userId != '00036'){    //测试 王超账号
-//                        continue;
-//                    }
+                    if($userId != '00153'){    //测试 账号
+                        continue;
+                    }
                     if(in_array($userId,$currentUserIds)){
                         continue;
                     }
@@ -248,7 +248,11 @@ class DingController extends Controller
                             $mainDingDepartmentForUser = $departmentIds[0];
                             DepartmentUser::updateAll(['is_main'=>1],['user_id'=>$kaelId,'depart_id'=>$mainDingDepartmentForUser]);
                         }
+                        print_r($departmentIds);
+                        echo $mainDingDepartmentForUser."#\n";
                         $relateKaelDepartmentId = self::getRelateKaelDepartment($mainDingDepartmentForUser);
+                        echo $relateKaelDepartmentId."#\n";
+                        exit('@@@@');
                         if($relateKaelDepartmentId && !$founder){
                             UserCenter::updateAll(['department_id'=>$relateKaelDepartmentId],['id'=>$kaelId]);
                         }elseif(!$founder){
