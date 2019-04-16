a51ab1afd780a5651fd062fd7a50d55ba20472f3
liqingshan <592957170@qq.com>
2019-04-16 18:38:31 +0800

    lqs

--- console/controllers/DingController.php
+++ console/controllers/DingController.php
@@ -83,12 +83,12 @@ class DingController extends Controller
                 ->asArray(true)->all();
             foreach ($departmentList as $v) {
                 $userIdList = DingTalkApi::getDepartmentUserIds($v['id']);
-                if($v['id']=='111705883'){
-                    print_r($userIdList);
-                    exit('ssss');
-                }else{
-                    continue;
-                }
+//                if($v['id']=='111705883'){
+//                    print_r($userIdList);
+//                    exit('ssss');
+//                }else{
+//                    continue;
+//                }
                 foreach ($userIdList as $userId){
 //                    if($userId != '00153'){    //测试 账号
 //                        continue;
