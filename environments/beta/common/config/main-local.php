76c88d1323b016cb678ca5d8f6f9ee56ccf60b3b
wangchao <wangchao@knowbox.cn>
2019-05-07 15:47:54 +0800

    swithqadb

--- environments/beta/common/config/main-local.php
+++ environments/beta/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [//用户中心
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=10.9.35.226;port=3339;dbname=usercenter',
+            'dsn' => 'mysql:host=172.16.2.176;port=3339;dbname=usercenter',
             'username' => 'test',
-            'password' => '123456',
+            'password' => 'qaOnly!@#',
             'charset' => 'utf8',
         ],
         'redis' => [
