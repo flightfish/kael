71126ef7c5d940b7bbb871b6380906035c49f862
wangchao <wangchao@knowbox.cn>
2019-07-10 15:32:09 +0800

    tmp

---
+++ environments/beta/common/libs/kmsclient/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: BPCoder
+ * Date: 2017/10/26
+ * Time: 12:15
+ */
+
+namespace common\libs\kmsclient;
+
+class Config {
+    public static $configServices = [
+        '172.16.1.197:8000',
+    ];
+    public static $kmsSource = "kael_inner_platform";
+}
