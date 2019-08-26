e388fef7a3cf2e872b8e0e951a0bdc35ddcb9993
wangchao <wangchao@knowbox.cn>
2019-08-26 11:37:23 +0800

    tmp

--- docs/sql/alimail.sql
+++ docs/sql/alimail.sql
@@ -1,6 +1,6 @@
-alter table `dingtalk_user`
-  add `email_created_ali` tinyint(4) NOT NULL DEFAULT '0' COMMENT '邮箱创建状态 0创建中 1已创建 2创建异常 3注销中 4已注销' AFTER `email_created`;
-
+-- alter table `dingtalk_user`
+--   add `email_created_ali` tinyint(4) NOT NULL DEFAULT '0' COMMENT '邮箱创建状态 0创建中 1已创建 2创建异常 3注销中 4已注销' AFTER `email_created`;
+--
 
 create table alimail_status (
 	id int not null auto_increment,
