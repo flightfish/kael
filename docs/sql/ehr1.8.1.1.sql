45b588365ebca6f77f4007c868861e16c9e4cbd7
liqingshan <592957170@qq.com>
2019-08-26 18:41:42 +0800

    lqs

--- docs/sql/ehr1.8.1.1.sql
+++ docs/sql/ehr1.8.1.1.sql
@@ -1,2 +1,2 @@
 alter table `dingtalk_department`
-add ehr_name varchar(255) not null default '' comment 'ehr名称' AFTER `name`;
+add alias_name varchar(255) not null default '' comment 'ehr名称' AFTER `name`;
