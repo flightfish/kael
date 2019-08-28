39405b38f50c079a26715e3c87f8546f5921dc50
liqingshan <592957170@qq.com>
2019-08-28 14:39:18 +0800

    lqs

--- common/components/mysql/MysqlCommand.php
+++ common/components/mysql/MysqlCommand.php
@@ -31,7 +31,7 @@ class MysqlCommand extends Command
         try {
             return parent::execute();
         } catch (\yii\db\Exception $e) {
-            var_dump($e);
+            var_dump($e->errorInfo);
             exit('###');
             if ($e->errorInfo[1] == 2006 || $e->errorInfo[1] == 2013) {
 //                echo '重连数据库';
