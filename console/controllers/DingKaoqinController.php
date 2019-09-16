42554ed86b9c8943839dadbec6def7c848836aa1
wangchao <wangchao@knowbox.cn>
2019-09-16 15:05:08 +0800

    tmp

--- console/controllers/DingKaoqinController.php
+++ console/controllers/DingKaoqinController.php
@@ -45,6 +45,42 @@ class DingKaoqinController extends Controller
     public function synKaoqin(){
         $ret = DingTalkApi::getAttendanceListRecord('2019-09-10 23:59:59','2019-09-16 23:59:59',['00036','15243079933019240','15667442833927047']);
         foreach ($ret['recordresult'] as $v){
+            /**
+             * {
+            "gmtModified": 1568599964000,
+            "baseCheckTime": 1568599200000,
+            "groupId": 160790019,
+            "timeResult": "Late",
+            "deviceId": "535321c9164b8fe9a75dc4fcc471b25",
+            "baseLongitude": 116.480198,
+            "userAccuracy": 550,
+            "classId": 80240005,
+            "workDate": 1568563200000,
+            "planId": 72491443063,
+            "id": 22752893818,
+            "baseAccuracy": 0,
+            "checkType": "OnDuty",
+            "planCheckTime": 1568599200000,
+            "corpId": "ding56f88c485c1f3d8e35c2f4657eb6378f",
+            "locationResult": "Normal",
+            "userLongitude": 116.474133,
+            "isLegal": "N",
+            "baseAddress": "阜安西路",
+            "gmtCreate": 1568599964000,
+            "userId": "15243079933019240",
+            "userAddress": "望京SOHO中心T2",
+            "userLatitude": 39.997954,
+            "baseLatitude": 39.997037,
+            "sourceType": "AUTO_CHECK",
+            "userCheckTime": 1568599964000,
+            "locationMethod": "MAP"
+            }
+             */
+            $v['gmtModified'] = date("Y-m-d H:i:s",intval($v['gmtModified']/1000));
+            $v['baseCheckTime'] = date("Y-m-d H:i:s",intval($v['baseCheckTime']/1000));
+            $v['workDate'] = date("Y-m-d H:i:s",intval($v['workDate']/1000));
+            $v['gmtCreate'] = date("Y-m-d H:i:s",intval($v['gmtCreate']/1000));
+            $v['userCheckTime'] = date("Y-m-d H:i:s",intval($v['userCheckTime']/1000));
             echo json_encode($v,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES)."\n";
         }
     }
