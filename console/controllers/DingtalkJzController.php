0e7e3d798e7cdb91e9dc5046947ba7758fe77c64
wangchao <wangchao@cwangmyth.cn>
2019-10-07 00:01:10 +0800

    tmp

--- console/controllers/DingtalkJzController.php
+++ console/controllers/DingtalkJzController.php
@@ -81,10 +81,9 @@ class DingtalkJzController extends Controller
 
 
     public function actionImportFromRows(){
-        $rows = require '/data/wwwroot/kael/rows.json';
+        $rows = file_get_contents('/data/wwwroot/kael/rows.json');
         $rows = json_decode($rows,true);
         $columns = ['mobile','name','department_name','department_id'];
-        var_dump($rows);exit();
         DBCommon::batchInsertAll(TmpImportJianzhi::tableName(),$columns,$rows,TmpImportJianzhi::getDb(),'INSERT IGNORE');
     }
 }
