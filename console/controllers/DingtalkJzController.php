f4f066403f7adad01118eb8c1ef481d2b1a5d334
wangchao <wangchao@cwangmyth.cn>
2019-10-06 23:12:57 +0800

    tmp

--- console/controllers/DingtalkJzController.php
+++ console/controllers/DingtalkJzController.php
@@ -11,6 +11,24 @@ class DingtalkJzController extends Controller
     public function actionTest(){
 
         $departmentList = DingTalkApiJZ::getDepartmentAllList();
+        $departmentList = array_column($departmentList,null,'id');
+        foreach ($departmentList as $k=>$v){
+            if(!empty($departmentList[$v['parentid']])){
+                $parentInfo = $departmentList[$v['parentid']];
+                if(!empty($departmentList[$parentInfo['parentid']])){
+                    $v['name3'] = $v['name'] . '|' . $parentInfo['name'] . '|' . $departmentList[$parentInfo['parentid']]['name'];
+                }else{
+                    $v['name2'] = $v['name'] . '|' . $parentInfo['name'];
+                }
+                $departmentList[$k] = $v;
+            }
+        }
+        $nameToInfo = [];
+        foreach ($departmentList as $k=>$v){
+            $name = $v['name3'] ?? $v['name2'] ?? $v['name'];
+            $nameToInfo[$name] = $v;
+        }
+        echo json_encode($nameToInfo,64|256)."\n";
         return $departmentList;
     }
 }
