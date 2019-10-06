4aa39a2e18d22800728b44eaa09fd61cb3e6d33e
wangchao <wangchao@cwangmyth.cn>
2019-10-06 23:34:05 +0800

    tmp

--- console/controllers/DingtalkJzController.php
+++ console/controllers/DingtalkJzController.php
@@ -10,13 +10,30 @@ class DingtalkJzController extends Controller
 {
     public function actionTest(){
 
+//        $PHPReader = new \PHPExcel_Reader_Excel2007(); // Reader很关键，用来读excel文件
+//        if (!$PHPReader->canRead($filePath)) { // 这里是用Reader尝试去读文件，07不行用05，05不行就报错。注意，这里的return是Yii框架的方式。
+//            $PHPReader = new \PHPExcel_Reader_Excel5();
+//            if (!$PHPReader->canRead($filePath)) {
+//                $errorMessage = "Can not read file.";
+//                array_push($error, $errorMessage);
+//                return $error;
+//            }
+//        }
+
+
+
         $departmentList = DingTalkApiJZ::getDepartmentAllList();
         $departmentList = array_column($departmentList,null,'id');
         foreach ($departmentList as $k=>$v){
             if(!empty($departmentList[$v['parentid']])){
                 $parentInfo = $departmentList[$v['parentid']];
                 if(!empty($departmentList[$parentInfo['parentid']])){
-                    $v['name3'] = $v['name'] . '|' . $parentInfo['name'] . '|' . $departmentList[$parentInfo['parentid']]['name'];
+                    $parentInfo2 = $departmentList[$parentInfo['parentid']];
+                    if(!empty($departmentList[$parentInfo2['parentid']])){
+                        $v['name4'] = $v['name'] . '|' . $parentInfo['name'] . '|' . $parentInfo2['name'] . '|' .$departmentList[$parentInfo2['parentid']]['name'];
+                    }else{
+                        $v['name3'] = $v['name'] . '|' . $parentInfo['name'] . '|' . $parentInfo2['name'];
+                    }
                 }else{
                     $v['name2'] = $v['name'] . '|' . $parentInfo['name'];
                 }
@@ -25,10 +42,32 @@ class DingtalkJzController extends Controller
         }
         $nameToInfo = [];
         foreach ($departmentList as $k=>$v){
-            $name = $v['name3'] ?? $v['name2'] ?? $v['name'];
+            $name = $v['name4'] ?? $v['name3'] ?? $v['name2'] ?? $v['name'];
             $nameToInfo[$name] = $v;
         }
-        echo json_encode($nameToInfo,64|256)."\n";
-        return $departmentList;
+
+        $filePath = './jianzhi_201909.xls';
+        $PHPReader = new \PHPExcel_Reader_Excel5();
+        $objPHPExcel = $PHPReader->load($filePath); // Reader读出来后，加载给Excel实例
+        $data = $objPHPExcel->getSheet(0)->toArray();
+        $dataFormat = [];
+        foreach ($data as $k=>$v){
+            if($k == 0){
+                echo json_encode($v,64|256);
+                continue;
+            }
+            if(empty($v[6])){
+                //没有手机号
+                continue;
+            }
+            //0工号忽略 1姓名 2.3.4.5 1-4级部门 6手机 7邮箱
+            $tmp = ['name'=>$v[1],'mobile'=>$v[6],'department_name'=>"{$v[5]}|{$v[4]}|{$v[3]}|{$v[2]}"];
+            $tmp['department_id'] = $nameToInfo[$tmp['department_name']]['id'];
+            $dataFormat[] = $tmp;
+        }
+
+        echo json_encode($dataFormat,64|256)."\n";
+
+        return $dataFormat;
     }
 }
