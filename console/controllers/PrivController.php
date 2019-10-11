5349cb5cc548ee8d252e745732c05f3d57ac0566
wangchao <wangchao@knowbox.cn>
2019-10-11 18:24:01 +0800

    auto open card 50004/5

--- console/controllers/PrivController.php
+++ console/controllers/PrivController.php
@@ -13,6 +13,7 @@ use yii\console\Controller;
 class PrivController extends Controller
 {
     public function actionBdp(){
+        //bdp 6000
         $sql = <<<SQL
 insert into relate_user_platform (user_id,platform_id)
 select DISTINCT a.id,6000
@@ -22,7 +23,7 @@ where a.`status` = 0 and a.user_type=0 and b.relate_id is null
 SQL;
         RelateUserPlatform::getDb()->createCommand($sql)->execute();
 
-
+        //okr
         $sql = <<<SQL
 insert into relate_user_platform (user_id,platform_id)
 select DISTINCT a.id,9001
@@ -31,6 +32,32 @@ from `user` a
 where a.`status` = 0 and a.user_type=0 and b.relate_id is null
 SQL;
         RelateUserPlatform::getDb()->createCommand($sql)->execute();
+
+
+        /**
+        boss 50004
+        io 50005
+        teacher not 50006
+         */
+        //boss
+        $sql = <<<SQL
+insert into relate_user_platform (user_id,platform_id)
+select DISTINCT a.id,50004
+from `user` a
+ left join relate_user_platform b on a.id=b.user_id and b.platform_id=50004
+where a.`status` = 0 and a.user_type=0 and b.relate_id is null and a.department_id=158
+SQL;
+        RelateUserPlatform::getDb()->createCommand($sql)->execute();
+        //io
+        $sql = <<<SQL
+insert into relate_user_platform (user_id,platform_id)
+select DISTINCT a.id,50005
+from `user` a
+ left join relate_user_platform b on a.id=b.user_id and b.platform_id=50005
+where a.`status` = 0 and a.user_type=0 and b.relate_id is null and a.department_id=158
+SQL;
+        RelateUserPlatform::getDb()->createCommand($sql)->execute();
+
 
     }
 }
