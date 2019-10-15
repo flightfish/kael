82938e4749ac9239682d8db74ce38887a52fc5e3
wangchao <wangchao@knowbox.cn>
2019-10-15 11:32:30 +0800

    tmp

--- console/controllers/PrivController.php
+++ console/controllers/PrivController.php
@@ -40,6 +40,7 @@ SQL;
         io 50005
         teacher not 50006
          */
+        /*
         //boss
         $sql = <<<SQL
 insert into relate_user_platform (user_id,platform_id)
@@ -57,6 +58,7 @@ from `user` a
  left join relate_user_platform b on a.id=b.user_id and b.platform_id=50005
 where a.`status` = 0 and a.user_type=0 and b.relate_id is null and a.department_id=158
 SQL;
+        */
         RelateUserPlatform::getDb()->createCommand($sql)->execute();
 
     }
@@ -91,6 +93,72 @@ SQL;
             echo $user['id']."\n";
             $ret = AppFunc::curlPost('http://bslive.online.knowboxlan.cn/permissionsMenu.do',[],$headers);
             echo $ret."\n";
+            sleep(1);
+        }
+    }
+
+
+    public function actionBossAllNew(){
+        if(exec('ps -ef|grep "priv/boss-all-new"|grep -v grep | grep -v cd | grep -v "/bin/sh"  |wc -l') > 1){
+            echo "is_running";
+            exit();
+        }
+        $sql = <<<SQL
+select DISTINCT a.*
+from `user` a
+ left join relate_user_platform b on a.id=b.user_id and b.platform_id=50004
+where a.`status` = 0 and a.user_type=0 and b.relate_id > 0 and a.department_id=158
+SQL;
+        $userList = CommonUser::getDb()->createCommand($sql)->queryAll();
+
+        foreach ($userList as $user){
+            $user['name'] = $user['username'];
+            $user['user_id'] = $user['id'];
+
+            $headers = [
+                "Referer: https://bslive.knowbox.cn/",
+                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+                "Sec-Fetch-Mode: cors",
+                "Accept: application/json, text/plain, */*"
+            ];
+            echo $user['id']."\n";
+            $ret = AppFunc::curlPost('https://bslive.knowbox.cn/employee/employeeValidateForKael.do',$user,$headers);
+            echo $ret."\n";
+            sleep(1);
+        }
+    }
+
+    public function actionBossUpdateNew(){
+        if(exec('ps -ef|grep "priv/boss-update-new"|grep -v grep | grep -v cd | grep -v "/bin/sh"  |wc -l') > 1){
+            echo "is_running";
+            exit();
+        }
+        $sql = <<<SQL
+select DISTINCT a.*
+from `user` a
+ left join relate_user_platform b on a.id=b.user_id and b.platform_id=50004
+where a.`status` = 0 and a.user_type=0 and b.relate_id is null and a.department_id=158
+SQL;
+        $userList = CommonUser::getDb()->createCommand($sql)->queryAll();
+
+        foreach ($userList as $user){
+            $user['name'] = $user['username'];
+            $user['user_id'] = $user['id'];
+
+            $headers = [
+                "Referer: https://bslive.knowbox.cn/",
+                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+                "Sec-Fetch-Mode: cors",
+                "Accept: application/json, text/plain, */*"
+            ];
+            echo $user['id']."\n";
+            $ret = AppFunc::curlPost('https://bslive.knowbox.cn/employee/employeeValidateForKael.do',$user,$headers);
+            echo $ret."\n";
+            $oldPlatIds = RelateUserPlatform::find()
+                ->select('platform_id')
+                ->where(['user_id'=>$user['id'],'status'=>0])->asArray(true)->column();
+            $addPlat = array_values(array_diff([50004,50005],$oldPlatIds));
+            !empty($addPlat) && RelateUserPlatform::batchAdd($user['id'],$addPlat);
             sleep(1);
         }
     }
