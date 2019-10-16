b33b8c61c17e7d868818047ad320034c1f177882
wangchao <wangchao@knowbox.cn>
2019-10-16 19:53:41 +0800

    tmp

--- usercenter/modules/admin/views/platform/index.php
+++ usercenter/modules/admin/views/platform/index.php
@@ -173,8 +173,8 @@
         pagination: true,          //是否显示分页（*）
         sidePagination: "server",      //分页方式：client客户端分页，server服务端分页（*）
         pageNumber:1,            //初始化加载第一页，默认第一页
-        pageSize: 1000,            //每页的记录行数（*）
-        pageList: [1000],    //可供选择的每页的行数（*）
+        pageSize: 10,            //每页的记录行数（*）
+        pageList: [10,20,50],    //可供选择的每页的行数（*）
         clickToSelect:false,        //是否启用点击选中行
         height: maxheight,         //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
         uniqueId: "id",           //每一行的唯一标识，一般为主键列
