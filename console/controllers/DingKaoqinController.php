28701178d9f09daebdacd5ee7265dd9e74a55c36
yubaozhu <243118745@qq.com>
2019-10-25 14:35:56 +0800

    tmp

--- console/controllers/DingKaoqinController.php
+++ console/controllers/DingKaoqinController.php
@@ -144,7 +144,7 @@ class DingKaoqinController extends Controller
 
     public function synSchedule($day){
         //排班时间
-        $scheduleList = DingTalkApi::getAttendanceListSchedule(date('Y-m-d',strtotime($day)));
+        $scheduleList = DingTalkApi::getAttendanceListSchedule(date('Y-m-d',strtotime($day)));return $scheduleList;
         $columns = [];
         $rows = [];
         foreach ($scheduleList as $v){
