e08ada0932e94becc21396e3b23193c7326f684d
wangchao <wangchao@knowbox.cn>
2019-10-28 19:28:44 +0800

    90003 wiki open

--- console/controllers/PrivController.php
+++ console/controllers/PrivController.php
@@ -34,6 +34,16 @@ where a.`status` = 0 and a.user_type=0 and b.relate_id is null
 SQL;
         RelateUserPlatform::getDb()->createCommand($sql)->execute();
 
+        //wiki
+        $sql = <<<SQL
+insert into relate_user_platform (user_id,platform_id)
+select DISTINCT a.id,90003
+from `user` a
+ left join relate_user_platform b on a.id=b.user_id and b.platform_id=90003
+where a.`status` = 0 and a.user_type=0 and b.relate_id is null
+SQL;
+        RelateUserPlatform::getDb()->createCommand($sql)->execute();
+
 
         /**
         boss 50004
