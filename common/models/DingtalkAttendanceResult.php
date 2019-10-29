9f3182a64867f7317bf7213724dc9940838ed42b
yubaozhu <243118745@qq.com>
2019-10-29 14:04:50 +0800

    tmp

--- common/models/DingtalkAttendanceResult.php
+++ common/models/DingtalkAttendanceResult.php
@@ -35,4 +35,14 @@ class DingtalkAttendanceResult extends \common\models\BaseActiveRecord
             DBCommon::batchInsertAll(self::tableName(),$columns,$rowsChunkOne,self::getDb(),'UPDATE');
         }
     }
+    public static function findListByWhereWithWhereArr($where,$whereArr,$select='*',$order='',$limit=0,$status=0){
+        !isset($where['status']) && $status != -1 && $where['status'] = $status;
+        $query =  self::find()->select($select)->where($where);
+        foreach ($whereArr as $v){
+            $query = $query->andWhere($v);
+        }
+        !empty($order) && $query = $query->orderBy($order);
+        $limit>0 && $query = $query->limit($limit);
+        return $query->asArray(true)->all();
+    }
 }
