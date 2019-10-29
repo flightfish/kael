d5cee8cd1baa1ef9e1546819838043c6cd2465f0
yubaozhu <243118745@qq.com>
2019-10-29 20:21:45 +0800

    tmp

--- console/controllers/MeicanController.php
+++ console/controllers/MeicanController.php
@@ -200,7 +200,7 @@ class MeicanController extends Controller
             echo "is_running";
             exit();
         }
-        var_dump(MeicanApi::listMember());return 1;
+        var_dump(MeicanApi::listRule());return 1;
         $dayList = array_map(function($v){
             return date("Y-m-d",$v);
         },range(strtotime('2019-09-01'),time(),24*3600));
