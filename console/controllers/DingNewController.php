f75020eefb14181e888bec6f36479d7a063bb1e6
wangchao <wangchao@knowbox.cn>
2019-11-08 16:28:16 +0800

    tmp

--- console/controllers/DingNewController.php
+++ console/controllers/DingNewController.php
@@ -363,6 +363,9 @@ SQL;
                     !empty($old) && DingtalkHrmUser::updateAll(['status'=>1],['id'=>$old['id']]);
                     continue;
                 }
+                if(substr($userField['sys00-mobile'],0,3) == '+86'){
+                    $userField['sys00-mobile'] = substr($userField['sys00-mobile'],3);
+                }
                 $updateParmas = [
                     'corp_type'=>$corpType,
                     'user_id'=>$resultUser['userid'],
