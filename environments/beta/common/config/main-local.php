5fbe2cb74ea99af9ca0efdeaa463a2fa73e9bc42
wangchao <wangchao@knowbox.cn>
2019-11-26 14:38:30 +0800

    tmp

--- environments/beta/common/config/main-local.php
+++ environments/beta/common/config/main-local.php
@@ -8,6 +8,13 @@ return [
             'password' => 'qaOnly!@#',
             'charset' => 'utf8',
         ],
+        'dbpool_live'=>[
+            'class' => 'yii\db\Connection',
+            'dsn'=>'mysql:host=172.16.2.176;port=3364;dbname=bukexuetang',
+            'username'=>'liyuan',
+            'password'=>'Liyuan@0123',
+            'charset'=>'utf8mb4',
+        ],
         'redis' => [
             'class' => 'usercenter\components\cache\Redis',
             'hostname' => '172.16.1.83',
